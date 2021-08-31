2a7a26c9b8f586b9230e659797bcc83ccac6c6d8
Ramok <steamxaiden@gmail.com>
2021-08-31 23:18:43 +0200

    sltcv

--- js/apipasgentil.js
+++ js/apipasgentil.js
@@ -1,1 +1,24 @@
 
+async function performLogin() {
+    var email = document.getElementById("emaillol");
+    var password = document.getElementById("passwd");
+    var request = await fetch('http://localhost:3000/', {
+        mode:'cors', 
+        credentials: 'include',
+        body: JSON.stringify({
+            email: email.value,
+            password: password.value
+        })
+    });
+
+    const response = await request.json();
+    const status = request.status;
+
+    if(status == 200) {
+        Notiflix.Notify.Success(`Vous êtes connecté, redirection dans quelques instants..\nBienvenue ${response.data.username}`);
+        console.log(response);
+        //window.location.href = "https://nlar.netlify.app";
+    } else {
+
+    }
+}
