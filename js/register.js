28e34831eb156ec24f23bd30cdf4754b4bbebb5c
RamokTVL <63268168+RamokTVL@users.noreply.github.com>
2021-10-06 17:36:53 +0200

    register

--- js/register.js
+++ js/register.js
@@ -1,11 +1,13 @@
 async function submit() {
     var eml = document.getElementById("email").value;
+    var usn = document.getElementById("usn").value;
     var psd = document.getElementById("psd").value;
 
     if(!eml || eml == "" || !/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(eml)) return alert("Invalid email");
     if(!psd || psd == "") return alert("Invalid password")
+    if(!usn || usn == "") return alert("Invalid password")
 
-    const req = await fetch("https://sltcv.herokuapp.com/login", {body: JSON.stringify({email: eml, password: psd}), method:"POST", mode:"cors", headers: {"Content-Type": "application/json"}});
+    const req = await fetch("https://sltcv.herokuapp.com/register", {body: JSON.stringify({email: eml, username: usn, password: psd}), method:"POST", mode:"cors", headers: {"Content-Type": "application/json"}});
   
     switch(req.status) {
         case 200:
@@ -17,11 +19,13 @@ async function submit() {
         case 429:
             alert("Veuillez réessayer plus tard.");
             break;
-        case 404:
-            alert("Votre compte n'existe pas.");
+        case 400:
+            var response = await req.json();
+            alert(response.data.message)
             break;
         case 403:
-            alert("Votre email ou votre mot de passe est invalide.");
+            var response = await req.json();
+            alert(response.data.message)
             break;
         case 500:
             alert("Internal server error");
