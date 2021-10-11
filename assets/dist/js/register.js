d136941044722edb79e25730a41e099784c9b4ca
RamokTVL <63268168+RamokTVL@users.noreply.github.com>
2021-10-11 23:41:31 +0200

    lmao

--- assets/dist/js/register.js
+++ assets/dist/js/register.js
@@ -1,15 +1,17 @@
 document.getElementById("submit").addEventListener("click", async() => {
     var eml = document.getElementById("email").value;
     var psd = document.getElementById("psd").value;
+    var usn = document.getElementById("floatingUsername").value;
 
     if(!eml || eml == "" || !/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(eml)) return alert("Invalid email");
     if(!psd || psd == "") return alert("Invalid password")
+    if(!usn || usn == "") return alert("Invalid username")
 
- const req = await fetch("https://sltcv.herokuapp.com/login", {body: JSON.stringify({email: eml, password: psd}), method:"POST", mode:"cors", headers: {"Content-Type": "application/json"}});
+ const req = await fetch("https://sltcv.herokuapp.com/register", {body: JSON.stringify({email: eml, password: psd, username: usn}), method:"POST", mode:"cors", headers: {"Content-Type": "application/json"}});
   
     switch(req.status) {
         case 200:
-            alert("Vous êtes connectés, bienvenue..");
+            alert("Vous êtes inscrit, bienvenue..");
             var response = await req.json();
             window.localStorage.setItem("token", response.data.token);
             window.location.pathname = "index.html";
